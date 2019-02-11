6c8681183713acb62199583a12c175152fc42fcb
Ryan <ryansteven.mueller@gmail.com>
2019-02-10 21:23:06 -0700

    api working with buttons

--- scripts/valor.js
+++ scripts/valor.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
-    $.get('https://cors-anywhere.herokuapp.com/http://valor-hs.ryansteven.com/Basement.json').done(function (data) {
+    $.get('https://cors-anywhere.herokuapp.com/http://valor-hs.ryansteven.com/Basement2.json').done(function (data) {
         console.log(data);
+        buttonClick(data);
     }).fail(function (data) {
         console.log('Error: ' + data);
     });
-
 })
 
-// on click of room, grab data-room-number, assign that as room in JSON file
+function buttonClick(data) {
+    $("button").on("click", function () {
+        var clickedRoom = $(this).attr("data-room-number")
+        for (let i = 0; i < data.length; i++) {
+            if (clickedRoom == data[i].room) {
+                $(".room").text("Room: " + data[i].room)
+                $(".door").text("Door Info: " + data[i].doorInfo)
+                $(".pin").text("Pinning: " + data[i].pinning)
+                $(".lever").text("Lever/Entry Hardware: " + data[i].leverHardware)
+            }
+        }
+    })
+}
