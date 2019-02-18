f437d5a9ae6ee0a5ba49dbce02bf1d82ae4096ae
Ryan <ryansteven.mueller@gmail.com>
2019-02-17 18:55:03 -0700

    buttons auto-generating

--- scripts/valor.js
+++ scripts/valor.js
@@ -1,22 +1,39 @@
 $(document).ready(function () {
     $.get('https://cors-anywhere.herokuapp.com/http://valor-hs.ryansteven.com/Basement2.json').done(function (data) {
         console.log(data);
+        createButtons(data);
         buttonClick(data);
     }).fail(function (data) {
         console.log('Error: ' + data);
     });
-})
 
-function buttonClick(data) {
-    $("button").on("click", function () {
-        var clickedRoom = $(this).attr("data-room-number")
-        for (let i = 0; i < data.length; i++) {
-            if (clickedRoom == data[i].room) {
-                $(".room").text("Room: " + data[i].room)
-                $(".door").text("Door Info: " + data[i].doorInfo)
-                $(".pin").text("Pinning: " + data[i].pinning)
-                $(".lever").text("Lever/Entry Hardware: " + data[i].leverHardware)
+    function buttonClick(data) {
+        $("button").on("click", function () {
+            var clickedRoom = $(this).attr("data-room-number")
+            for (let i = 0; i < data.length; i++) {
+                if (clickedRoom == data[i].room) {
+                    $(".room").html(`<h2>Room: <span class='badge badge-primary'>${data[i].room}</span>`)
+                    $(".door").html(`<h2>Door Info: <span class='badge badge-primary'>${data[i].doorInfo}</span>`)
+                    $(".pin").html(`<h2>Pinning: <span class='badge badge-primary'>${data[i].pinning}</span>`)
+                    $(".lever").html(`<h2>Lever/Entry Hardware: <span class='badge badge-primary'>${data[i].leverHardware}</span>`)
+                    
+                    // $(".room").text("Room: " + data[i].room)
+                    // $(".door").text("Door Info: " + data[i].doorInfo)
+                    // $(".pin").text("Pinning: " + data[i].pinning)
+                    // $(".lever").text("Lever/Entry Hardware: " + data[i].leverHardware)
+                }
             }
+        })
+    }
+    
+    function createButtons(data) {
+        for (var i = 0; i < data.length; i++) {
+            var newBtn = $("<button>");
+            newBtn.text(data[i].room);
+            newBtn.addClass("btn btn-success m-2 grnButton p-3");
+            newBtn.attr("data-room-number", data[i].room);
+            $(".valor-cards").append(newBtn);
         }
-    })
-}
+    }
+    
+})
